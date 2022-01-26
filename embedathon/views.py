a43a2af9f9d62a402d9773e8de4fecbbb5b225bb
Nishant Nayak <nishantnayak2001@gmail.com>
2022-01-26 20:02:29 +0530

    Add register function
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- embedathon/views.py
+++ embedathon/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World")
+
+def register(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+
+    # create a new user
+    user = User.objects.create_user(username, email, password)
