c5937eb12a7695d2d74ba49c4bcec0bd5ac21b1b
Nishant Nayak <nishantnayak2001@gmail.com>
2022-01-29 21:02:40 +0530

    Update static assets URL
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- embedathon_website/settings.py
+++ embedathon_website/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://ieee.nitk.ac.in/embedathon-static-assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
