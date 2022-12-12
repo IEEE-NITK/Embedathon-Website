36b8ae74e8093e6bcce65a28efa248d4fe518d81
Nishant Nayak <66667161+nishant-nayak@users.noreply.github.com>
2022-12-13 00:45:45 +0530

    Add export functionality (#39)
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- embedathon_website/settings.py
+++ embedathon_website/settings.py
@@ -40,6 +40,7 @@ AUTH_USER_MODEL = 'embedathon.User'
 
 INSTALLED_APPS = [
     'embedathon',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
