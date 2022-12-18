3990de7e717f88e94be96112d0a10c89f60f96a8
Nishant Nayak <nishantnayak2001@gmail.com>
2022-12-19 00:36:04 +0530

    Add testing email views
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- embedathon_website/settings.py
+++ embedathon_website/settings.py
@@ -151,3 +151,10 @@ HACKATHON_START = False
 
 # IMPORTANT: Set this value to True when the Registration Starts
 REGISTRATION_START = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
